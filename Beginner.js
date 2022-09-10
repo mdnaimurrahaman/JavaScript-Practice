9354ac5988b3c4b92b1186a5b906922c2b53e887
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-10 21:53:12 +0600

    Exercise Q 1 done

---
+++ Beginner.js
@@ -0,0 +1,24 @@
+/*
+JavaScript Practice Exercises For Beginners
+===========================================
+
+Beginner Exercises: Part 1
+--------------------------
+*/
+
+const { compose } = require("@reduxjs/toolkit");
+
+/* 
+Exercise 1:
+-----------
+Write a JavaScript program to check tow numbers and return true 
+if one of the number is 100 or if the sum of the tow numbers is 100.
+*/
+
+/*
+const isEqualTotal = (a, b) => a === 100 || b === 100 || (a+b) === 100;
+console.log(isEqualTotal(100,0))
+console.log(isEqualTotal(0,100))
+console.log(isEqualTotal(50,50))
+*/
+
