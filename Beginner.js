5a1f6165edbfbd114aa34aad194b89a1ebcfe0b5
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-10 22:04:27 +0600

    Exercise 2 solution

--- Beginner.js
+++ Beginner.js
@@ -6,8 +6,6 @@ Beginner Exercises: Part 1
 --------------------------
 */
 
-const { compose } = require("@reduxjs/toolkit");
-
 /* 
 Exercise 1:
 -----------
@@ -16,9 +14,23 @@ if one of the number is 100 or if the sum of the tow numbers is 100.
 */
 
 /*
+Solution:
 const isEqualTotal = (a, b) => a === 100 || b === 100 || (a+b) === 100;
 console.log(isEqualTotal(100,0))
 console.log(isEqualTotal(0,100))
 console.log(isEqualTotal(50,50))
 */
+
+/*
+Exercise 2
+----------
+Write a javaScript program to get the extension of a filename.
+*/
+/*
+Solution:
+const getFileExtension = (start) => start.slice(start.lastIndexOf('.'))
+console.log(getFileExtension('index.module.css'))
+*/
+
+
 
